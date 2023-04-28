b52e52f7d1a5e678d0c178e8afd5ad74cf7e2162
Vale-pixel <1005896964@u.icesi.edu.co>
2023-04-27 19:33:02 -0500

    Add nft list page.

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+const home = document.querySelector('.home');
+const nftlist = document.querySelector('.nft-list');
+setButtons()
 
 const carousel = document.querySelector('.twitter-carousel');
 const comment = carousel.querySelectorAll('.comment');
@@ -11,3 +14,22 @@ let interval = setInterval(() => {
   }
   comment[index].style.opacity = 1;
 }, 2000);
+
+function setButtons() {
+  const buttonHome = document.querySelector('.home-button')
+  const buttonNftList = document.querySelector('.nft-list-button')
+  console.log(buttonNftList);
+  buttonHome?.addEventListener('click', ()=> goToHome())
+  buttonNftList?.addEventListener('click', ()=> goToNftList())
+
+}
+function goToHome() {
+  nftlist.classList.add('hidden')
+  home.classList.remove('hidden')
+  setButtons()
+}
+function goToNftList() {
+  nftlist.classList.remove('hidden')
+  home.classList.add('hidden')
+  setButtons()
+}
