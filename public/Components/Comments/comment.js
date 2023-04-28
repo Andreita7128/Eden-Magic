5b72afb6b6e855b08b0907e6e94aa978a636567d
Andrea Torrente <andrea.torrente1@u.icesi.edu.com>
2023-04-28 01:47:42 -0500

    component comment ready

--- public/Components/Comments/comment.js
+++ public/Components/Comments/comment.js
@@ -1,27 +1,49 @@
+export var AttributeComment;
+(function (AttributeComment) {
+    AttributeComment["pictureProfile"] = "pictureProfile";
+    AttributeComment["userName"] = "userName";
+    AttributeComment["comment"] = "comment";
+    AttributeComment["likes"] = "likes";
+})(AttributeComment || (AttributeComment = {}));
 class Comment extends HTMLElement {
     constructor() {
         super()
-}
+    }
 
-connectedCallback() {
-    this.render()
-}
+    static get observedAttributes() {
+        const attrs = {
+            pictureProfile: null,
+            userName: null,
+            comment: null,
+            likes: null,
+        };
+        return Object.keys(attrs);
+    }
 
-render() {
-    this.innerHTML = `
+    connectedCallback() {
+        this.render()
+    }
+
+    attributeChangedCallback(propName, oldValue, newValue) {
+        this[propName] = newValue;
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
     <link rel="stylesheet" href="../../../public/Components/Comments/comment.css">
     <article id="comment">
-            <p id="comment_user">User Name</p>
+            <p id="comment_user"> ${this.userName}</p>
             <section id="comment_body">
-                <img id="comment_img" src="/img/images comments/Ellipse 7.png" alt="">
+                <img id="comment_img" src=" ${this.pictureProfile} ">
                 <div id="comment_text">
-                    <p id="comment_comment">Escribe aquí tu texto</p>
+                    <p id="comment_comment"> ${this.comment} </p>
                 </div>
             </section>
             <section id="comment_reactions">
                 <div id="comment_likes">
                     <i class="bi bi-heart"></i>
-                    <p id="comment_number">140</p>
+                    <p id="comment_number"> ${this.likes}</p>
                 </div>
                 <i class="bi bi-three-dots"></i>
             </section>
@@ -29,8 +51,16 @@ render() {
     
 
     `
-}
+    }
 
+    renderEmpty() {
+        this.innerHTML = `
+        <link rel="stylesheet" href="../../../public/Components/Comments/comment.css">
+        <article id="comment">
+            <p id="comment_noComment"> ¡Parece que está un poco vacío por acá, sé el primero en comentar! </p>
+            </article>
+    `
+    }
 }
 
 customElements.define('comments-product', Comment)
