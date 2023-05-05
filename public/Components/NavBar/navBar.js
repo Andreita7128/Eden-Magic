8cbc31426e4ba0475a2d9b209bcd0ca54c36346f
Andreita <andrea.torrente1@u.icesi.edu.co>
2023-05-05 08:13:55 -0500

    si se pudo

--- public/Components/NavBar/navBar.js
+++ public/Components/NavBar/navBar.js
@@ -16,11 +16,11 @@ class NavBar extends HTMLElement {
             <input type="search" class="inputSearch" placeholder="Search collections and creators">
             <ul>
                 <!---Cada # se le tiene que asignar a la sección-->
-                <li><a class="home-button" href="#home">Home</a></li>
+                <li><a class="home-button" href="../../../landing.html">Home</a></li>
                 <li><a href="#launchpad">Launchpad</a></li>
                 <li><a href="#auctions">Auctions</a></li>
                 <li><a href="#stats">Stats</a></li>
-                <li><a class="nft-list-button" href="#nft-list-products">Top Collections</a></li>
+                <li><a class="nft-list-button" href="../../../public/Components/AuctionCards/auctionCard.html">Top Collections</a></li>
             </ul>
             <a href="https://transacciones.nequi.com/bdigital/login.jsp" class="btn_header">Connect Wallet</a>
         </nav>
